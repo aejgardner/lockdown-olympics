0f6d7fe3671fab3e521b1858ce04e3466aec9dff
Adam Gardner <adam.gardner4991@gmail.com>
2020-06-15 16:18:46 +0100

    Categories controller setup, API request for categories list working

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Categories;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// CATEGORIES
+Route::group(["prefix" => "/categories"], function () {
+
+    // list all categories
+    Route::get('', [Categories::class, "index"]);
 });
