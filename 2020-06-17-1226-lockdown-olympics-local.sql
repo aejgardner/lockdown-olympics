52c14d85540bcc43ca8fe0aa083c10ac3a86577c
Adam Gardner <adam.gardner4991@gmail.com>
2020-06-17 12:28:28 +0100

    database dump done containing updated category name (Mental to extreme) and banana bread sports description edited

---
+++ 2020-06-17-1226-lockdown-olympics-local.sql
@@ -0,0 +1,225 @@
+-- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: homestead
+-- ------------------------------------------------------
+-- Server version	5.7.29-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `img_pathway` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'2020-06-15 13:22:26','2020-06-17 10:13:42','Extreme','__'),(2,'2020-06-16 17:17:31','2020-06-16 17:17:31','Home',''),(3,'2020-06-16 17:18:02','2020-06-16 17:18:02','Garden',''),(4,'2020-06-16 17:18:13','2020-06-16 17:18:13','Food','');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `competitors`
+--
+
+DROP TABLE IF EXISTS `competitors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `competitors` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bio` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dob` date NOT NULL,
+  `sport_id` bigint(20) unsigned NOT NULL,
+  `img_pathway` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `competitors_sport_id_foreign` (`sport_id`),
+  CONSTRAINT `competitors_sport_id_foreign` FOREIGN KEY (`sport_id`) REFERENCES `sports` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `competitors`
+--
+
+LOCK TABLES `competitors` WRITE;
+/*!40000 ALTER TABLE `competitors` DISABLE KEYS */;
+INSERT INTO `competitors` VALUES (1,'2020-06-15 13:48:24','2020-06-15 13:48:24','Gary Barlow','It didn\'t take that much training for this ladies man to become adept at bog snorkelling. This man clearly appreciates a mud bath','1971-01-20',1,''),(2,'2020-06-15 13:52:45','2020-06-16 13:49:14','Julius Caesar','Who would\'ve thought Rome\'s greatest emperor would have a secret boggy talent! Catch him hitting the mud like nobody\'s business','1790-07-12',1,'__'),(3,'2020-06-16 12:55:35','2020-06-16 12:55:35','Nigella Lawson','I\'m not just good at cooking!','1960-01-06',2,'genericpathway'),(4,'2020-06-16 13:22:20','2020-06-16 13:53:54','Will Smith','TURN IT OVER AND HIT IT, OOO NA NA NAAAA','1968-09-25',2,'generic_pathway'),(5,'2020-06-16 20:55:31','2020-06-16 20:55:31','Mr Blobby','Polka dot? Yes. Washed up? Maybe. but that won\'t stop this humanoid giving his non existent hair the best trim ever!','1360-02-04',3,'genericpathway'),(6,'2020-06-16 20:58:11','2020-06-16 20:58:11','Gandalf','It\'s been 65 years since this middle earther took some clippers to his barnet, see him make a return for the ages.','1945-08-12',3,'genericpathway'),(7,'2020-06-16 21:02:32','2020-06-16 21:02:32','Pingu','Noot Noot.','1987-05-02',4,'genericpathway'),(8,'2020-06-16 21:07:53','2020-06-16 21:07:53','Henry Hoovery','Watch this machine hurl himself down the stairs he once hoovered so gracefully.','1924-07-30',4,'genericpathway'),(9,'2020-06-16 21:23:48','2020-06-16 21:23:48','Chuck Norris','Chuck sends his most prized worm into the ring, carnage ensues.','1967-09-20',5,'genericpathway'),(10,'2020-06-16 21:28:27','2020-06-16 21:28:27','David Beckham','David\'s worm plans to bend a right hook into the top right of his opponent worm\'s segments.','1985-04-10',5,'genericpathway'),(11,'2020-06-16 21:34:57','2020-06-16 21:34:57','Florence Nightingale','Watch one of history\'s greatest nurses nurse these plants to perfection.','1985-04-10',6,'genericpathway'),(12,'2020-06-16 21:40:19','2020-06-16 21:40:19','ABBA','Dancing queen extraordinaires exchange roller blades for watering cans and give some plants a good old soak.','1962-02-11',6,'genericpathway'),(13,'2020-06-16 21:49:17','2020-06-16 21:49:17','Barack Obama','Having some more time on his hands these days, Barack discovered a newfound joy of making everyones favourite bananery treat. Watch him put his baking skills to the ultimate test.','1953-07-19',7,'genericpathway'),(14,'2020-06-16 22:34:01','2020-06-16 22:34:01','Shrek','Amongst whipping up a mean fish-eye tata and rat tail stew, our favourite loveable ogre takes to the bread.','1670-09-12',7,'genericpathway'),(15,'2020-06-16 22:42:57','2020-06-16 22:42:57','Barney the Dinosaur','Didn\'t know your favourite purple dinosaur had a penchant for shooting peas through a straw? We didn\'t either! But find him partkaing in such an event at our games.','1980-10-28',8,'genericpathway'),(16,'2020-06-16 22:46:19','2020-06-16 22:46:19','Napoleon','From shooting cannons to shooting peas, this man\'s career has taken a step in the right direction.','1629-03-24',8,'genericpathway');
+/*!40000 ALTER TABLE `competitors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+DROP TABLE IF EXISTS `failed_jobs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `failed_jobs`
+--
+
+LOCK TABLES `failed_jobs` WRITE;
+/*!40000 ALTER TABLE `failed_jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `failed_jobs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migrations`
+--
+
+DROP TABLE IF EXISTS `migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migrations`
+--
+
+LOCK TABLES `migrations` WRITE;
+/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
+INSERT INTO `migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1),(3,'2019_08_19_000000_create_failed_jobs_table',1),(4,'2020_06_15_114337_create_categories_table',1),(5,'2020_06_15_115231_create_sports_table',2),(6,'2020_06_15_131408_create_competitors_table',3),(7,'2020_06_16_105906_add_img_pathway_field_to_sports_table',4),(8,'2020_06_16_112613_add_img_pathway_field_to_competitors_table',5),(9,'2020_06_17_093342_add_img_pathway_to_categories_table',6);
+/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `password_resets`
+--
+
+DROP TABLE IF EXISTS `password_resets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `password_resets`
+--
+
+LOCK TABLES `password_resets` WRITE;
+/*!40000 ALTER TABLE `password_resets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `password_resets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sports`
+--
+
+DROP TABLE IF EXISTS `sports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sports` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `start_time` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `arena` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category_id` bigint(20) unsigned NOT NULL,
+  `img_pathway` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sports_category_id_foreign` (`category_id`),
+  CONSTRAINT `sports_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sports`
+--
+
+LOCK TABLES `sports` WRITE;
+/*!40000 ALTER TABLE `sports` DISABLE KEYS */;
+INSERT INTO `sports` VALUES (1,'2020-06-15 13:27:50','2020-06-17 10:51:11','Bog snorkelling','Dive head first into a wonderful pit of watery mud','Friday 13:30','Olympic',1,'__'),(2,'2020-06-15 13:43:09','2020-06-15 13:43:09','Giant Pumpkin Kayaking','If you\'re tired of using a kayak to go kayaking, then why not give this quirky sport a try instead? The most famous giant pumpkin race by far takes place annually on Nova Scotia\'s Lake Pesaquid. The race is around half a mile long, and participants typically decorate their pumpkins in a colorful way.','Saturday 12:00','Pumpkin',1,''),(3,'2020-06-16 17:40:42','2020-06-16 17:40:42','Self-inflicted hairdressing','Competitors put their new found hairstyling skills to the test. Who will be the fastest to give themselves a passable haircut?','Sunday 16:30','Mirror',2,'generic'),(4,'2020-06-16 17:46:30','2020-06-16 17:46:30','Stair Tobogganing','Who needs ice and a mountain when you have stairs and a duvet? Watch as competitors speed down to the bottom of the stairs','Saturday 19:30','Home Sweet Home',2,'generic'),(5,'2020-06-16 17:51:02','2020-06-16 17:51:02','Worm Fighting','The world\'s most skillful worm handlers compete for the title. Those worms are more aggressive than you might think!','Monday 12:00','Garden World',3,'generic'),(6,'2020-06-16 17:53:30','2020-06-16 17:53:30','Competitive plant watering','Who can be the fastest to water the garden? No hoses allowed!','Sunday 12:00','Garden World',3,'generic'),(7,'2020-06-16 18:00:27','2020-06-17 11:24:04','The Banana Bread Cup','It\'s the most popular recipe of lockdown, but who\'s will be crowned the best? And will there be enough flour?','Monday 16:30','Home Sweet Home',4,'generic'),(8,'2020-06-16 18:05:44','2020-06-16 18:05:44','Pea Shooting','Competitors shoot peas 12 feet through the air at a 12 inch target, in the ultimate test of skill and accuracy. Not to be missed','Friday 19:30','Olympic',4,'generic');
+/*!40000 ALTER TABLE `sports` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-17 11:26:49
